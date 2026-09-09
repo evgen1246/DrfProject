eefccc9f54c6afeff34d6e01022b69eefdf0cfc3
Evgen <evgen1246@yandex.ru>
2026-09-09 20:31:17 +0300

    валидарор-проверка. модель подписки

--- learnix/serializers.py
+++ learnix/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from .models import Course, Lesson
+from .models import Course, Lesson, Subscription
+from .validators import validate_youtube_only
 
 
 class LessonSerializer(serializers.ModelSerializer):
     """Сериализатор для уроков"""
+    video_url = serializers.URLField(
+        validators=[validate_youtube_only],
+        required=False,
+        allow_blank=True,
+        allow_null=True
+    )
 
     class Meta:
         model = Lesson
@@ -17,6 +24,7 @@ class LessonSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         ]
+        read_only_fields = ["owner"]
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -37,7 +45,16 @@ class CourseSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         ]
+        read_only_fields = ["owner"]
 
     def get_lessons_count(self, obj):
         """Возвращает количество уроков в курсе"""
         return obj.lessons.count()
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    """Сериализатор для подписки"""
+
+    class Meta:
+        model = Subscription
+        fields = ["id", "user", "course", "created_at"]
+        read_only_fields = ["id", "user", "created_at"]
